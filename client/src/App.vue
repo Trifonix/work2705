360f734ef80219d20b9567626085500ba5d175f3
Trifonix <92928912+Trifonix@users.noreply.github.com>
2024-05-29 15:10:25 +0300

    upd app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  
+  <div id="app">
+    <h1>Тестирование socket.io + vue</h1>
+    <button @click="sendMessage">Отправить сообщение</button>
+  </div>
 </template>
 
 <script>
+export default {
+  name: 'App',
+  mounted() {
+    this.$socket.on('connect', () => {
+      console.log('Успешное подключение к серверу');
+    });
 
+    this.$socket.on('disconnect', () => {
+      console.log('Отключение от сервера');
+    });
+  },
+  methods: {
+    sendMessage() {
+      console.log('Отправляем сообщение на сервер...');
+      this.$socket.emit('message', 'Привет, я клиент!');
+    }
+  }
+};
 </script>
 
 <style>
